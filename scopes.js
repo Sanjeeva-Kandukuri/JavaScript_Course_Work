ec6273f1d037b87defbd05ced94c099bba11a063
Sanjeev <ksanjeeva225@gmail.com>
2026-03-25 19:26:28 +0530

    Add files via upload

---
+++ scopes.js
@@ -0,0 +1,57 @@
+// let greet = "Hello Good Evening"
+
+                                       
+// console.log(greet)                           // global scope we can use inside and outside 
+
+// function add(){
+//     console.log("Satish", greet)
+
+
+// }
+// add();
+
+// function localScope(){
+//     let name = "Local Scope"
+//     console.log(name);
+// }
+// localScope();
+// console.log();   //console.log(name);
+
+
+// if(true){
+//     var value = "Black Scope"     // let var global scope ==>  const
+//     console.log(value)
+// }
+// console.log(value);
+
+
+// function outer(){
+//     let value_col = "Clouser's will work"
+//     function inner(){
+//             console.log(value_col)
+        
+//     }
+//     return inner();
+// }
+
+// outer();
+
+// IIFE functions
+
+(function(){
+    console.log("Immediately Invoked function Expression")
+}()) 
+
+
+
+greeting("dhanya")
+
+function greeting(name){
+    console.log("Hello", name)
+}
+
+console.log(b)
+var b = 20;
+
+
+
