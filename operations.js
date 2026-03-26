e7d6fe77676d1ad546a229ba1de7f1a62beeaced
Sanjeev <ksanjeeva225@gmail.com>
2026-03-26 23:13:00 +0530

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,77 @@
+// String operations
+
+let value = " Python Full Stack "
+console.log(value.toLowerCase());
+console.log(value.toUpperCase());
+console.log(value.trimEnd());      // end , start,
+// console.log(value.trim());
+console.log(value.replace('Python', 'Java'));
+console.log(value.indexOf('Stack'));
+console.log(value.length);
+console.log(value.slice(0, 8));
+console.log(value.split(" "));
+let split = value.split(" ")
+console.log(split.join("*"));
+console.log(split.includes('Python'));
+console.log(value.substring(4, 9));
+console.log(value.repeat(5));
+
+let greet = 'Hi';
+
+
+ let palindrome =  'madam';                                  // join, reverse, split
+ let result = palindrome.split("").reverse().join("");
+console.log(result);
+
+
+if(palindrome === result){
+    console.log(result, "is palindrome")
+}
+else{
+    console.log(result, "is Not palindrome")
+}
+
+
+
+
+let num = 4;
+console.log(Math.floor(num));
+
+let num_1 = 5.4;
+console.log(Math.ceil(num_1));
+
+
+let num_2 = 1.6;
+console.log(Math.round(num_2));
+
+let power = 4
+console.log(Math.pow(power, 3));
+
+let sqrt = 25;
+console.log(Math.sqrt(sqrt))
+
+
+let random = Math.floor(Math.random()*10 + 1);
+console.log(random);
+
+
+// Date Objects();
+
+let date = new Date
+console.log(date);
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getMonth() +1)    // +1 optional
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
+
+
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleTimeString());
+
+
+
