2d81e2dc975c80316f20e78d2f559cbc72c1cecd
Sanjeev <ksanjeeva225@gmail.com>
2026-03-26 23:07:51 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,35 @@
+function greeting(){                    // function declaration
+    console.log('Hello Good Morning')
+}
+
+greeting()
+
+
+let add = function greet(){             // function expression
+    console.log("Hi Good Evening")
+}
+
+add();
+
+
+let adding = function(){                // Ananymous function
+    console.log(10 + 25)
+}
+
+adding();
+
+// adding()
+
+
+
+let adding_1 = function(){
+    console.log(10 + 25)
+}
+adding_1()
+
+
+function sum(){
+    console.log(10 + 20)
+}
+sum()
+
