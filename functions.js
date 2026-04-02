d2919cf83d1d8eb6d5cdc65cae332732d1cab8f6
Sanjeev <ksanjeeva225@gmail.com>
2026-04-02 23:54:53 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,18 +1,18 @@
-function greeting(){                    // function declaration
+function greeting(){                    
     console.log('Hello Good Morning')
 }
 
 greeting()
 
 
-let add = function greet(){             // function expression
+let add = function greet(){             
     console.log("Hi Good Evening")
 }
 
 add();
 
 
-let adding = function(){                // Ananymous function
+let adding = function(){                
     console.log(10 + 25)
 }
 
