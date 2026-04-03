fa02529223b0bec3e3b2fe1284fc79e8057cfebf
Sanjeev <ksanjeeva225@gmail.com>
2026-04-03 23:37:39 +0530

    Add files via upload

---
+++ tempLiterals.js
@@ -0,0 +1,36 @@
+let name = 'Anish' ;
+let age = 22;
+console.log(`My name is ${name}`);
+console.log(`My age ${age} & My name is ${name}`);
+//console.log(name + age);
+
+let wish = "Hello Good  morning"
+function greeting(){
+    console.log(`${wish}`)
+
+}
+
+greeting();
+
+
+function sum(){
+    let a = 10;
+    let b = 30;
+    console.log(`Sum of ${a} and ${b} is ${a+b}`)
+}
+
+sum();
+
+
+let text = `Fisrt Line
+ Second Line
+  Third Line
+     Fourth line`;
+
+console.log(text);
+
+
+function welcome(student){
+    console.log(`${student} Sai groups`);
+}
+welcome("Welcome To The comapny");
